3daaef44b317bb2b23def0d75e10a52f9dec45e4
LABCONTROL\421-12 <danimh110@gmail.comm>
2022-08-22 08:42:06 -0500

    add gitignore

---
+++ Tareas/Programas/Memoria dinamica/dynamic_memory.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	
+	int *iptr;
+	
+	iptr = (int *)malloc(sizeof(int));
+	
+	if (iptr == NULL)
+		return -1;
+	
+	*iptr =5;
+	
+	printf("%d, %p", *iptr, iptr);
+	
+	free(iptr);
+	
+	return 0;
+}
+
